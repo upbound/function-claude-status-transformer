26474ac8551eaebca0f640e967d5dbd349d236ca
tnthornton <2375126+tnthornton@users.noreply.github.com>
2025-08-20 13:21:01 -0700

    provide authentication helpers for AWS and Anthropic

---
+++ internal/credentials/anthropic/anthropic.go
@@ -0,0 +1,61 @@
+// /*
+// Copyright 2025 The Upbound Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// */
+
+package anthropic
+
+import (
+	"strings"
+
+	"github.com/crossplane/crossplane-runtime/pkg/errors"
+	fnv1 "github.com/crossplane/function-sdk-go/proto/v1"
+	"github.com/crossplane/function-sdk-go/request"
+	"github.com/crossplane/function-sdk-go/resource"
+)
+
+const (
+	credName = "claude"
+	credKey  = "ANTHROPIC_API_KEY"
+)
+
+// Anthropic provides API Key access.
+type Anthropic struct {
+	req *fnv1.RunFunctionRequest
+}
+
+// New constructs a new Anthropic.
+func New(req *fnv1.RunFunctionRequest) *Anthropic {
+	return &Anthropic{req: req}
+}
+
+// GetAPIKey retrieves the API key from the incoming RunFunctionRequest.
+func (a *Anthropic) GetAPIKey() (string, error) {
+	c, err := request.GetCredentials(a.req, credName)
+	if err != nil {
+		return "", errors.Wrapf(err, "cannot get Anthropic API key from credential %q", credName)
+	}
+	if c.Type != resource.CredentialsTypeData {
+		return "", errors.Errorf("expected credential %q to be %q, got %q", credName, resource.CredentialsTypeData, c.Type)
+	}
+	b, ok := c.Data[credKey]
+	if !ok {
+		return "", errors.Errorf("credential %q is missing required key %q", credName, credKey)
+	}
+	// TODO(negz): Where the heck is the newline at the end of this key
+	// coming from? Bug in crossplane render?
+	key := strings.Trim(string(b), "\n")
+
+	return key, nil
+}
--- internal/credentials/aws/aws.go
+++ internal/credentials/aws/aws.go
@@ -17,50 +17,81 @@
 package aws
 
 import (
+	"context"
+	"maps"
+
 	"github.com/aws/aws-sdk-go-v2/aws"
 
+	corev1 "k8s.io/api/core/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
 	"github.com/crossplane/crossplane-runtime/pkg/errors"
+	"github.com/crossplane/crossplane-runtime/pkg/test"
 	fnv1 "github.com/crossplane/function-sdk-go/proto/v1"
 	"github.com/crossplane/function-sdk-go/request"
 	"github.com/crossplane/function-sdk-go/resource"
 
 	"github.com/upbound/function-claude-status-transformer/input/v1beta1"
+	"github.com/upbound/function-claude-status-transformer/internal/credentials/aws/clients"
 )
 
-const (
-	secretName = "aws"
-	secretKey  = "credentials"
-)
-
+// AWS provides AWS specific credential access.
 type AWS struct {
-	valid bool
+	c client.Client
+
+	cfg *v1beta1.AWS
 }
 
+// New creates a new AWS.
 func New(in *v1beta1.StatusTransformation, req *fnv1.RunFunctionRequest) *AWS {
-	if in.AWS == nil {
-		return &AWS{}
+	a := &AWS{
+		c:   &fnCredClient{req: req},
+		cfg: in.AWS,
 	}
-
-	_ = *in.AWS
-	return &AWS{}
+	return a
 }
 
-func (a *AWS) GetConfig() aws.Config {
-	return aws.Config{}
+// GetConfig returns an aws.Config derived from the request context and
+// environment.
+func (a *AWS) GetConfig(ctx context.Context) (*aws.Config, error) {
+	return clients.GetAWSConfig(ctx, a.c, a.cfg)
 }
 
-func getRequestCredentials(req *fnv1.RunFunctionRequest) ([]byte, error) {
-	c, err := request.GetCredentials(req, secretName)
-	if err != nil {
-		return nil, errors.Wrapf(err, "cannot get Anthropic API key from credential %q", secretName)
-	}
-	if c.Type != resource.CredentialsTypeData {
-		return nil, errors.Errorf("expected credential %q to be %q, got %q", secretName, resource.CredentialsTypeData, c.Type)
-	}
-	b, ok := c.Data["credentials"]
+var _ client.Client = &fnCredClient{}
+
+// fnCredClient is a simple client that embeds the upstream test.MockClient in
+// order to statisfy the client.Client interface contract. It has only one
+// purpose, that is to redirect requests to Secrets back to the incoming
+// Function Request so that we do not need to provide access to the API server.
+type fnCredClient struct {
+	req *fnv1.RunFunctionRequest
+
+	test.MockClient
+}
+
+// Get mocks the standard client.Client get call to pull the Secret retrieval
+// from the incoming function request, rather than going to kube. This enables
+// us to utilize some helpers from c/crossplane-runtime without needing to give
+// the function a client with API server access.
+func (c *fnCredClient) Get(ctx context.Context, key client.ObjectKey, obj client.Object, _ ...client.GetOption) error {
+	s, ok := obj.(*corev1.Secret)
 	if !ok {
-		return nil, errors.Errorf("credential %q is missing required key %q", secretName, secretKey)
+		return errors.New("invalid object Kind supplied for retrieval, should be Secret but was not")
 	}
 
-	return b, nil
+	s.SetName(key.Name)
+	s.SetNamespace(key.Namespace)
+
+	creds, err := request.GetCredentials(c.req, key.Name)
+	if err != nil {
+		return errors.Wrapf(err, "cannot retrieve credential from Secret with name %q", key.Name)
+	}
+	if creds.Type != resource.CredentialsTypeData {
+		return errors.Errorf("expected credential %q to be %q, got %q", key.Name, resource.CredentialsTypeData, creds.Type)
+	}
+
+	// copy data from the creds map to the secret
+	maps.Copy(s.Data, creds.Data)
+
+	return nil
 }
